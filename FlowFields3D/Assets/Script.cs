80066cc22493f3a02efd42bf207f07a4c50a89cc
Logist- <pavelkrot@gmail.com>
2014-01-02 19:10:45 +0400

    First Try
    Катящийся кубик

---
+++ FlowFields3D/Assets/Script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script : MonoBehaviour {
+
+	GameObject[] cub = new GameObject[10];
+	// Use this for initialization
+	void Start () {
+		cub = GameObject.FindGameObjectsWithTag ("Cub");
+		cub[0].transform.position = new Vector3 (5, 5, 5);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
