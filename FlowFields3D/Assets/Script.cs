0333b20a5f70f9c74fb2c45f2d515febc0e3fc37
Randamm <hoos.spb@mail.ru>
2014-01-02 20:26:59 +0400

    http://robotics.bstu.by/mwiki/images/4/47/PathFinding.pdf

--- FlowFields3D/Assets/Script.cs
+++ FlowFields3D/Assets/Script.cs
@@ -5,14 +5,27 @@ public class Script : MonoBehaviour {
 
 	GameObject[] cub = new GameObject[10];
 	// Use this for initialization
+	Vector3 finish  = new Vector3(35,2,35);
+	Vector3 start = new Vector3(0,2,0);
+	float dist;
 	void Start () {
 		cub = GameObject.FindGameObjectsWithTag ("Cub");
-		cub[0].transform.position = new Vector3 (5, 5, 5);
-	
+		cub [0].transform.position = start;
+		
 	}
-	
+
+	void distansce(){
+		dist = Mathf.Sqrt (Mathf.Pow (finish.x - cub [0].transform.position.x, 2) + Mathf.Pow (finish.z - cub [0].transform.position.z, 2));
+	}
+	Vector3 magnitude(){
+		Vector3 force = new Vector3 (Mathf.Abs((finish.x - cub [0].transform.position.x)) * ((finish.x - cub [0].transform.position.x) / dist), 0,
+		                             Mathf.Abs((finish.z - cub [0].transform.position.z)) * ((finish.z - cub [0].transform.position.z) / dist));
+		return force;
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		distansce ();
+		cub[0].constantForce.force = magnitude ();
+		//cub[0].constantForce.force = new Vector3(magnitude ().x * 0.1F , 0, magnitude ().z * 0.1F);
 	}
 }
