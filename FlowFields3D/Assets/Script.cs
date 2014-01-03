77fd4a58cadcfb0da79aa6a7988db303185792b1
Randamm <hoos.spb@mail.ru>
2014-01-03 17:06:38 +0400

    added new rules for magnitude

--- FlowFields3D/Assets/Script.cs
+++ FlowFields3D/Assets/Script.cs
@@ -5,8 +5,10 @@ public class Script : MonoBehaviour {
 
 	public static GameObject[] cub;
 	public static Vector3 finish  = new Vector3(35,2,35);
+	//public static Vector3 finish2  = new Vector3(35,2,35);
 	float dist;
 	public static float alpha=0.5f;
+	public static float eps=0.6f;
 	void Start () {
 		cub = GameObject.FindGameObjectsWithTag ("Cub");
 	}
@@ -21,9 +23,13 @@ public class Script : MonoBehaviour {
 			}
 			else {
 				tmp.transform.constantForce.force = tmp.transform.constantForce.force + new Vector3(
-					(-1*(st.transform.position.z - tmp.transform.position.z)/distR)*alpha,
+					-1*(((st.transform.position.z - tmp.transform.position.z)/distR)*alpha)
+					/
+					(distR/((eps*alpha)+1)),
 					0,
-					((st.transform.position.x - tmp.transform.position.x)/distR)*alpha);
+					(((st.transform.position.x - tmp.transform.position.x)/distR)*alpha)
+					/
+					(distR/((eps*alpha)+1)));
 			}
 
 		}
