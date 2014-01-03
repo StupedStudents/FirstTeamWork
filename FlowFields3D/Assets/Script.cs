4c573b0b6748f6d6b02c11599fba86e4059a4460
Randamm <hoos.spb@mail.ru>
2014-01-03 16:47:13 +0400

    configured settings for normal influence

--- FlowFields3D/Assets/Script.cs
+++ FlowFields3D/Assets/Script.cs
@@ -6,7 +6,7 @@ public class Script : MonoBehaviour {
 	public static GameObject[] cub;
 	public static Vector3 finish  = new Vector3(35,2,35);
 	float dist;
-
+	public static float alpha=0.5f;
 	void Start () {
 		cub = GameObject.FindGameObjectsWithTag ("Cub");
 	}
@@ -16,12 +16,14 @@ public class Script : MonoBehaviour {
 		foreach (GameObject st in Script.cub) {
 			distR = Mathf.Sqrt (Mathf.Pow (st.transform.position.x - tmp.transform.position.x, 2) +
 			                           Mathf.Pow (st.transform.position.z - tmp.transform.position.z, 2));
-			if(distR > 10 || st == tmp) {
+			if(distR > 2 || st == tmp) {
 				continue;
 			}
 			else {
-				st.transform.constantForce.force = new Vector3(0,100,0);
-				tmp.transform.constantForce.force = new Vector3(0,100,0);
+				tmp.transform.constantForce.force = tmp.transform.constantForce.force + new Vector3(
+					(-1*(st.transform.position.z - tmp.transform.position.z)/distR)*alpha,
+					0,
+					((st.transform.position.x - tmp.transform.position.x)/distR)*alpha);
 			}
 
 		}
